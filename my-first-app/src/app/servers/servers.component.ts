11e8d075464f036c7925df0ceb939e0560c331af
Nilesh Pawar <nilesh.pawar@bharti-consulting-services.com>
2023-06-17 15:43:11 +0530

    Basic Practice

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  /*selector: '[app-server]',*/ /* attribute selector*/
+  /*selector: '.app-server',*/
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+}
